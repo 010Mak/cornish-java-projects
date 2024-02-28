a9e210a781cfba5ba0d2a1e1533a284e9d668eff
010Mak <96219805+010Mak@users.noreply.github.com>
2024-02-27 21:26:12 -0500

    Create IntegerMathOpsEx6.java

---
+++ MathEx5-6/Ex6/IntegerMathOpsEx6.java
@@ -0,0 +1,29 @@
+public class IntegerMathOpsEx6 {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 10;
+
+
+        a += b;
+        System.out.println("adding b to a, a = " + a);
+
+
+        a -= b;
+        System.out.println("subtracting b from a, a = " + a);
+
+
+        a *= b;
+        System.out.println("multiplying a by b, a = " + a);
+
+
+        a /= b;
+        System.out.println("dividing a by b, a = " + a);
+
+
+        a %= 8;
+        System.out.println("a mod 8, a = " + a);
+
+
+    }
+}
