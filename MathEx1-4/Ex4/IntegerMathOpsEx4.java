4354053d3c8c6f197120ff0cb9ccd35c5519e12e
010Mak <96219805+010Mak@users.noreply.github.com>
2024-02-27 22:53:38 -0500

    Update IntegerMathOpsEx4.java

---
+++ MathEx1-4/Ex4/IntegerMathOpsEx4.java
@@ -0,0 +1,21 @@
+public class IntegerMathOpsEx4 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 10;
+
+        a = a + b;
+        System.out.println("a: " + a);
+
+        a = a - b;
+        System.out.println("a: " + a);
+
+        a = a * b;
+        System.out.println("a: " + a);
+
+        a = a / b;
+        System.out.println("a: " + a);
+
+        a = a % 8;
+        System.out.println("a: " + a);
+    }
+}
