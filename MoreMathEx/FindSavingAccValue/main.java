47d4beffe9fe0352e50cb83db086e1aa6e41a5ab
010Mak <96219805+010Mak@users.noreply.github.com>
2024-03-04 14:02:29 -0500

    Create main.java

---
+++ MoreMathEx/FindSavingAccValue/main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+double p = 12000;
+double r = 2.5;
+double n = 52;
+double t = 18;
+
+  
+double a  = p * Math.pow(1 + r / n, n * t);
+
+
+  System.out.println(a);
+  
+}
+}
