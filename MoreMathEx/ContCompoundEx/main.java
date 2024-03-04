3b32b3ad9b9e9f88a360db06285c813f119e11cc
010Mak <96219805+010Mak@users.noreply.github.com>
2024-03-04 14:43:37 -0500

    Create main.java

---
+++ MoreMathEx/ContCompoundEx/main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+      double p = 10000;
+      double r = 2.75;
+      double t = 8;
+      
+      double a = p * Math.pow(e, r * t);
+}
+}
