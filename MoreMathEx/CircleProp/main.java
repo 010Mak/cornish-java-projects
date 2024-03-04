d5fdb6dc1beb97b7739c83d9d4999174072c623e
010Mak <96219805+010Mak@users.noreply.github.com>
2024-03-04 12:47:13 -0500

    Update main.java

--- MoreMathEx/CircleProp/main.java
+++ MoreMathEx/CircleProp/main.java
@@ -3,7 +3,7 @@ public class Main {
         double radius = 16;
         double area = Math.PI * radius * radius;
         double circumference = 2 * Math.PI * radius;
-        System.out.println("Area is: " + area);
-        System.out.println("Circumference is: " + circumference);
+        System.out.println(area);
+        System.out.println(circumference);
     }
 }
