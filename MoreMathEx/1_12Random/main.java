1d02c8bbab5f6e8403a8189db29d35e0925392f6
010Mak <96219805+010Mak@users.noreply.github.com>
2024-03-04 13:54:31 -0500

    Create main.java

---
+++ MoreMathEx/1_12Random/main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            int random = (int)(Math.random() * 12) + 1;
+            System.out.println(random);
+        }
+    }
+}
