1013c96a90761a88ad2ade09ac1792a612177449
010Mak <96219805+010Mak@users.noreply.github.com>
2024-03-04 12:46:43 -0500

    Update main.java

---
+++ MoreMathEx/CircleProp/main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double radius = 16;
+        double area = Math.PI * radius * radius;
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("Area is: " + area);
+        System.out.println("Circumference is: " + circumference);
+    }
+}
