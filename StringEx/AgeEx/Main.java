d3b4a8b89a476397e7b26511af1a14f898774027
010Mak <96219805+010Mak@users.noreply.github.com>
2024-03-06 14:54:58 -0500

    Create Main.java

---
+++ StringEx/AgeEx/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+      Scanner input = new Scanner(System.in);
+      
+
+      System.out.println("person 1");
+
+      String Mak = input.nextLine();
+
+      System.out.println("mak is 15");
+
+      System.out.println("person 2");
+
+      String Brian = input.nextLine();
+
+      System.out.println("Brian is 15");
+
+      System.out.println("person 3");
+
+      String Iris = input.nextLine();
+
+      System.out.println("Iris is 16");
+
+
+     
+  
+}
+}
