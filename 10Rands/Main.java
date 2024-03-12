0b7b792ebf17dc345c719f1fc09ce2f5576b67cc
010Mak <96219805+010Mak@users.noreply.github.com>
2024-03-12 11:00:44 -0400

    Update Main.java

---
+++ 10Rands/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TenRandomNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter minimum number (num1): ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter maximum number (num2): ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Random number 1: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+        System.out.println("Random number 2: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+        System.out.println("Random number 3: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+        System.out.println("Random number 4: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+        System.out.println("Random number 5: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+        System.out.println("Random number 6: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+        System.out.println("Random number 7: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+        System.out.println("Random number 8: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+        System.out.println("Random number 9: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+        System.out.println("Random number 10: " + ((int)(Math.random() * (num2 - num1 + 1) + num1)));
+
+        scanner.close();
+    }
+}
