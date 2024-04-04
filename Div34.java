cab4ccda0cf9657a94c909be37218c051614309d
010Mak <96219805+010Mak@users.noreply.github.com>
2024-04-04 14:24:03 -0400

    Create Div34.java

---
+++ Div34.java
@@ -0,0 +1,16 @@
+public class Div34 {
+  public static void main(String[] args) {
+
+      int counter = 0;
+
+
+      for (int i = 1; i <= 100; i++) {
+
+          if (i % 12 == 0) {
+      }
+
+
+      System.out.println(counter);
+  }
+  }
+}
