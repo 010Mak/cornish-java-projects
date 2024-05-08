53c052d0a58333575d46657c14759f1b83c89311
010Mak <96219805+010Mak@users.noreply.github.com>
2024-05-07 23:18:45 -0400

    Create ArraySwap1.java

---
+++ ArraySwap1.java
@@ -0,0 +1,8 @@
+public class ArraySwap1 {
+    public static void main(String[] args) {
+        char[] a = {'z', 'x', 'y'};
+        char temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+    }
+}
