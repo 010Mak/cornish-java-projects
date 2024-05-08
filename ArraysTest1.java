9f58d777d67e6e96c5aed3e6f42a91305a4cbf5b
010Mak <96219805+010Mak@users.noreply.github.com>
2024-05-07 23:17:39 -0400

    Create ArraysTest1.java

---
+++ ArraysTest1.java
@@ -0,0 +1,9 @@
+public class ArraysTest1 {
+    public static void main(String[] args) {
+        int[] houseNum = new int[20];
+        int[] byTen = {5, 10, 15, 20, 25, 30};
+        double[] interest = new double[5];
+        String[] street = {"Maple Street", "Oak Lane", "Wisteria Avenue", "Cherry Circle"};
+        char[] grades = new char[25];
+    }
+}
