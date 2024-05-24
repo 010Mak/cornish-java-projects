b071189c57d5a32b7211b924f4ed5633d28ed719
Mak <96219805+010Mak@users.noreply.github.com>
2024-05-24 14:26:21 -0400

    Update NameGuess.java

---
+++ NameGuess.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NameGuess {
+    public static void main(String[] args) {
+        String[] names = {"Iris", "Brian", "Mak", "Seb", "Sean", "Will", "Rishaan"};
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        String chosenName = names[random.nextInt(names.length)];
+        char[] maskedName = new char[chosenName.length()];
+        for (int i = 0; i < maskedName.length; i++) {
+            maskedName[i] = '*';
+        }
+
+        System.out.println("Guess the name: " + new String(maskedName));
+
+        while (new String(maskedName).contains("*")) {
+            System.out.print("Enter a letter: ");
+            char guessedLetter = scanner.nextLine().charAt(0);
+
+            boolean found = false;
+            for (int i = 0; i < chosenName.length(); i++) {
+                if (chosenName.charAt(i) == guessedLetter) {
+                    maskedName[i] = guessedLetter;
+                    found = true;
+                }
+            }
+
+            if (!found) {
+                System.out.println("Letter not in the name.");
+            }
+
+            System.out.println(maskedName);
+        }
+
+        System.out.println("Well done! The name was: " + chosenName);
+        scanner.close();
+    }
+}
